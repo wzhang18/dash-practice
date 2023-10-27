cb14f395a6030b637c490232ea652bd8b5e04ba3
wzhang18 <66753347+wzhang18@users.noreply.github.com>
2023-10-26 23:08:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,53 @@ Created on Thu Oct 26 08:58:55 2023
 """
 from dash import Dash, dcc, html, callback, Input, Output
 import pandas as pd
+import plotly.express as px
 
-data = pd.read_csv('random sample data.csv')
+data = pd.read_csv('https://raw.githubusercontent.com/wzhang18/dash-practice/main/random%20sample%20data.csv')
 
 app = Dash(__name__)
 server = app.server # required for render.com deployment
 
+#df = data[data['Position'].isin(['HR', 'CEO'])]
+#groupeddf = pd.crosstab(df['Position'], df[df.columns[21]], normalize = 'index')
+#fig2 = px.bar(groupeddf, x=groupeddf.index, y = groupeddf.columns, text_auto= '.1%')
+
 app.layout = html.Div([
-    dcc.Markdown('''# Sample Report ''')
-    
+    dcc.Markdown('''# Sample Report '''),
+    dcc.Dropdown(
+        options = data['Position'].unique(),
+        value = [],
+        multi = True,
+        id = 'position'
+        ),
+    dcc.Dropdown(
+        options = data.columns[21:-1],
+        value = (),
+        id = 'question'
+        ),
+    #dcc.Graph(
+     #   figure = fig2
+      #  ),
+    dcc.Graph(
+        id = 'graph'
+        )
     ])
+
+@callback(
+    Output(component_id = 'graph', component_property = 'figure'),
+    Input(component_id = 'position', component_property = 'value'),
+    Input(component_id= 'question', component_property= 'value'),
+    prevent_initial_call=True 
+    )
+def graph(positions, question):
+    df = data[data['Position'].isin(positions)]
+    groupeddf = pd.crosstab(df['Position'], df[question], normalize = 'index')
+    fig = px.bar(groupeddf, x=groupeddf.index, y = groupeddf.columns, text_auto= '.1%')
+    
+    #df = data[data['Position'].isin(positions)]
+    #fig = px.pie(df, names = question)
+    return fig
+
 
 if __name__ == '__main__':
     app.run(debug = True)
